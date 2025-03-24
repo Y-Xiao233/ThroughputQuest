d30ea335fcb2882e980e17a10771624a4bb90177
Y-Xiao233 <1870377449@qq.com>
2025-03-24 13:39:10 +0800

    v1.0.0

--- src/main/java/net/yxiao233/throughputquest/ThroughputQuest.java
+++ src/main/java/net/yxiao233/throughputquest/ThroughputQuest.java
@@ -1,6 +1,7 @@
 package net.yxiao233.throughputquest;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.yxiao233.throughputquest.common.ftbq.TQTaskTypes;
@@ -10,8 +11,7 @@ import net.yxiao233.throughputquest.common.registries.TQItems;
 @Mod(ThroughputQuest.MODID)
 public class ThroughputQuest {
     public static final String MODID = "throughput_quest";
-    public ThroughputQuest() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public ThroughputQuest(IEventBus modEventBus, ModContainer modContainer) {
         TQItems.DR.register(modEventBus);
         TQCreativeModeTab.CREATIVE_MODE_TAB.register(modEventBus);
         TQTaskTypes.init();
